ab104f53a44ac5835a5d8d3472367d7a86ff6c9e
rookied <rookiezq@icloud.com>
2021-08-06 22:29:25 +0800

    加入扫描所有mapper接口，项目启动成功

--- src/main/java/com/rookied/FamilyServicePlatformApplication.java
+++ src/main/java/com/rookied/FamilyServicePlatformApplication.java
@@ -1,9 +1,11 @@
 package com.rookied;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan
 public class FamilyServicePlatformApplication {
 
     public static void main(String[] args) {
