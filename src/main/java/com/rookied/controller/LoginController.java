65f00777f8799d3ee75ab69a2a6a1d1f8b0e8508
rookied <rookiezq@icloud.com>
2021-08-07 11:08:40 +0800

    后台获取到username和password

--- src/main/java/com/rookied/controller/LoginController.java
+++ src/main/java/com/rookied/controller/LoginController.java
@@ -1,24 +1,27 @@
 package com.rookied.controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * @author zhangqiang
  * @date 2021/8/6
  */
 @RestController
-@CrossOrigin(origins = "*", allowCredentials = "true", allowedHeaders = "*", methods = {RequestMethod.POST})
 public class LoginController {
 
     //这是二次验证的请求，可以不用理会
     @RequestMapping("/auth/2step-code")
-    public Boolean stepCode(){
+    public Boolean stepCode() {
         return true;
     }
 
     @RequestMapping("/auth/login")
-    public String login() {
-        System.out.println("login");
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        System.out.println(username + "--" + password);
         return "login";
     }
 }
