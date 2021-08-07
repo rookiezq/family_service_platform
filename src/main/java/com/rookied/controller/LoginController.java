b16bd184898d654a74a68eae99f84c6cd0dca369
rookied <rookiezq@icloud.com>
2021-08-07 08:51:43 +0800

    二次验证

--- src/main/java/com/rookied/controller/LoginController.java
+++ src/main/java/com/rookied/controller/LoginController.java
@@ -10,6 +10,12 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*", allowCredentials = "true", allowedHeaders = "*", methods = {RequestMethod.POST})
 public class LoginController {
 
+    //这是二次验证的请求，可以不用理会
+    @RequestMapping("/auth/2step-code")
+    public Boolean stepCode(){
+        return true;
+    }
+
     @RequestMapping("/auth/login")
     public String login() {
         System.out.println("login");
